ff8b0df2927e635da26f27cf989cbc0edffee8af
Christopher Brandt <christopher.brandt@autocloud.dev>
2022-05-09 14:35:13 -0700

    chore: add service alias

--- src/enums/serviceAliases.ts
+++ src/enums/serviceAliases.ts
@@ -3,6 +3,7 @@ export default {
   securityGroup: 'securityGroups',
   ccn: 'ccns',
   ccnAttachment: 'ccnAttachments',
+  securityGroup: 'securityGroups',
   subnet: 'subnets',
   vpc: 'vpcInstances',
   vpnGateway: 'vpnGateways',
